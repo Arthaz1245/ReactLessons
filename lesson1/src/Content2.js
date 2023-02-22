515461f0d5bafe60589b20e833c500649b0c11eb
Arthaz1245 <sergiomendozap96@gmail.com>
2023-02-22 17:32:38 -0500

    tutorial pt 2

--- lesson1/src/Content2.js
+++ lesson1/src/Content2.js
@@ -12,23 +12,43 @@ const Content2 = () => {
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setItems(listItems);
+    //local storage to save information when its reload
+    localStorage.setItem("shoppnglist", JSON.stringify, listItems);
+  };
+  const handleDelete = (id) => {
+    const itemDeleted = items.filter((item) => item.id !== id);
+    setItems(itemDeleted);
+    localStorage.setItem("shoppnglist", JSON.stringify, itemDeleted);
   };
   return (
     <main>
-      <ul>
-        {items.map((item) => (
-          <li className="item" key={item.id}>
-            <input
-              type="checkbox"
-              onChange={() => handleCheck(item.id)}
-              checked={item.checked}
-            />
-            {item.item}
-            <label>{item.item}</label>
-            <FaTrashAlt role="button" tabIndex="0" />
-          </li>
-        ))}
-      </ul>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+              />
+
+              <label
+                style={item.checked ? { color: "#A3E419" } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+              >
+                {item.item}
+              </label>
+              <FaTrashAlt
+                onClick={() => handleDelete(item.id)}
+                role="button"
+                tabIndex="0"
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list is empty</p>
+      )}
     </main>
   );
 };
