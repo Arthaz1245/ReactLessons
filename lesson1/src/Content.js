c10a726aa5f6d416c62e5bf721d50a7767f7144c
Arthaz1245 <sergiomendozap96@gmail.com>
2023-02-22 16:19:54 -0500

    tutorial pt 2

--- lesson1/src/Content.js
+++ lesson1/src/Content.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 const Content = () => {
+  const [product, setProduct] = useState("Apple");
+  const [count, setCount] = useState(0);
   const handleGroceryProducts = () => {
     const products = ["Apple", "Watermelon", "Orange", "Potatoes"];
     const int = Math.floor(Math.random() * 4);
-    return products[int];
+    setProduct(products[int]);
+  };
+  const handleClick = () => {
+    console.log("You clicked it");
+  };
+  const handleCountinc = () => {
+    setCount(count + 1);
+  };
+  const handleCountdec = () => {
+    setCount(count - 1);
   };
   return (
     <main>
-      <p>You want {handleGroceryProducts()}</p>
+      <p onDoubleClick={handleClick}>You want {product}</p>
+      <button onClick={handleGroceryProducts}>Click it</button>
+      <button onClick={handleCountdec}>-</button>
+      {count}
+      <button onClick={handleCountinc}>+</button>
     </main>
   );
 };
