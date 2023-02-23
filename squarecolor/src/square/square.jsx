47fdd9ad1d4cdbbbf7ee55e602d65819a6fc6d0d
Arthaz1245 <sergiomendozap96@gmail.com>
2023-02-23 18:25:00 -0500

    Commit checkpoint

--- squarecolor/src/square/square.jsx
+++ squarecolor/src/square/square.jsx
@@ -2,9 +2,14 @@ import "./square.css";
 const square = ({ color }) => {
   return (
     <main>
-      <div className="square" style={{ backgroundColor: `${color}` }}></div>
+      <div className="square" style={{ backgroundColor: `${color}` }}>
+        <p>{color ? color : "Empty Value"}</p>
+      </div>
     </main>
   );
+};
+square.defaultProps = {
+  color: "Empty Value",
 };
 
 export default square;
